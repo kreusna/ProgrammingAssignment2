97668addc0a8171e5ef6980acd2ec0eac9d671eb
Ly Kreusna <ly.kreusna@gmail.com>
2021-06-01 15:53:01 +0700

    update assignment function

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,26 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  ## m to NULL
+  m <- NULL
+  ## create set() and store matrix to x
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  ## create get() return x
+  get <- function() x
+  
+  ## create setInverse() and set inverse matrix to m
+  setInverse <- function(inverse) m <<- inverse 
+  
+  ## create getInverse() and return m
+  getInverse <- function() m
+  
+  ## create list for function
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse) 
 }
 
 
@@ -12,4 +31,23 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  ## get inverse matrix 
+  m <- x$getInverse()
+  
+  ## check if inverse matrix is set
+  if(!is.null(m)){
+    return(m)
+  }
+  
+  ## get matrix 
+  data <- x$get()
+  
+  ## matrix is inverse 
+  m <- solve(data)
+  
+  ## set inverse matrix
+  x$setInverse(m)
+  
+  ## return inverse matrix
+  m
 }
